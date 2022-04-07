1e98e4dbb5d27317b5cd6876215058064d51cc94
AConstant31 <alexjconstantino31@gmail.com>
2022-04-06 20:59:45 -0400

    Adding more space for Alex section

--- js/index.js
+++ js/index.js
@@ -1,1 +1,32 @@
-/*
+// Alex's Task 4 section
+
+
+console.log(tasks);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//End of Alex's Task 4 section
+
--- js/taskManager.js
+++ js/taskManager.js
@@ -1,10 +1,31 @@
 //Task 4 Alex making TaskManager Class
 
+let tasks;
+
+class taskManager {
+  
+    constructor(tasks) {
+      this.tasks = [];
+    }
+  
+  }
 
 
 
 
 
 
-//End of Alex's TaskManager Class section
+
+
+
+
+
+
+
+
+
+
+
+
+  //End of Alex's TaskManager Class section
 
