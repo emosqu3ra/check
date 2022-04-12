59c8bb3e31a773aa00137975a35348b3c6f5781b
E L Ortiz <elortiz@Es-MacBook-Air.local>
2022-04-11 20:30:23 -0400

    working on taskManager.js.1

--- js/index.js
+++ js/index.js
@@ -1,52 +1,51 @@
-// Select the New Task Form
-// const newTaskForm = document.querySelector('#newTaskForm');
-document.getElementById("newTaskForm")
+// // Select the New Task Form
+// // const newTaskForm = document.querySelector('#newTaskForm');
+// document.getElementById("newTaskForm")
 
-// Add an 'onsubmit' event listener
-newTaskForm.addEventListener('submit', (event) => {
-    // Prevent default action
-    event.preventDefault();
+// // Add an 'onsubmit' event listener
+// newTaskForm.addEventListener('submit', (event) => {
+//     // Prevent default action
+//     event.preventDefault();
 
-    // Select the inputs
-    const newTaskNameInput = document.querySelector('#newTaskNameInput');
-    const newTaskDescription = document.querySelector('#newTaskDescription');
-    const newTaskAssignedTo = document.querySelector('#newTaskAssignedTo');
-    const newTaskDueDate = document.querySelector('#newTaskDueDate');
-    const errorMessage = document.querySelector('#alertMessage');
+//     // Select the inputs
+//     const newTaskNameInput = document.querySelector('#newTaskNameInput');
+//     const newTaskDescription = document.querySelector('#newTaskDescription');
+//     const newTaskAssignedTo = document.querySelector('#newTaskAssignedTo');
+//     const newTaskDueDate = document.querySelector('#newTaskDueDate');
+//     const errorMessage = document.querySelector('#alertMessage');
     
-    /*
-        Validation code here
-    */
+//     /*
+//         Validation code here
+//     */
 
-    // Get the values of the inputs
-    const name = newTaskNameInput.value;
-    const description = newTaskDescription.value;
-    const assignedTo = newTaskAssignedTo.value;
-    const dueDate = newTaskDueDate.value;
-    if(!validFormFieldInput(name)){
-        errorMessage.innerHTML = "Invalid name input";
-        errorMessage.style.display = "block"
-    }else{
-        errorMessage.style.display = "none"
-    }
+//     // Get the values of the inputs
+//     const name = newTaskNameInput.value;
+//     const description = newTaskDescription.value;
+//     const assignedTo = newTaskAssignedTo.value;
+//     const dueDate = newTaskDueDate.value;
+//     if(!validFormFieldInput(name)){
+//         errorMessage.innerHTML = "Invalid name input";
+//         errorMessage.style.display = "block"
+//     }else{
+//         errorMessage.style.display = "none"
+//     }
 
-});
+// });
 
-function validFormFieldInput(data){
-    return data !== null && data !== '';
-}
+// function validFormFieldInput(data){
+//     return data !== null && data !== '';
+// }
 
 
-//End of Alex's Task 4 section
 
-// Estephanie Task 7
-const taskslist = document.querySelector('#tasksList');
+// // Estephanie Task 7
+// const taskslist = document.querySelector('#tasksList');
 
-taskslist.addEventListener('click', doneButtonChecker);
+// taskslist.addEventListener('click', doneButtonChecker);
 
-function doneButtonChecker() {
+// function doneButtonChecker() {
    
-}
+// }
 
 
 
